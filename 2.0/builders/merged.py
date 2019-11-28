0e84243c0d456ef1693d9a386d9f178ec66d67da
Jacob Windsor <me@jcbwndsr.com>
2019-11-28 15:58:13 +0100

    Perform merging in a functional way

--- 2.0/builders/merged.py
+++ 2.0/builders/merged.py
@@ -1,24 +1,23 @@
 from models.contig import Contig
 from models.transcript import Transcript
+from functools import reduce
 
 def build(contigs):
     return list(map(merge, contigs))
 
 def merge(contig):
-    contig = Contig(merge_monoexonic(contig.transcripts))
-    return contig
+    transcripts = reduce(merge_monoexonic, contig.transcripts, [contig.transcripts[0]])
+    return Contig(transcripts)
 
-def merge_monoexonic(transcripts, i=0):
-    if i >= len(transcripts) -1:
-        return transcripts
-    
-    if transcripts[i].exons[0].end > transcripts[i+1].exons[0].start:
+def merge_monoexonic(transcripts, to_merge):
+    if len(transcripts) == 0:
+        return [to_merge]
+
+    last_transcript = transcripts[len(transcripts) -1]
+    if last_transcript.exons[0].end > to_merge.exons[0].start:
         # Overlapping so merge
-        transcripts[i].add_exon(transcripts[i+1].exons[0])
+        last_transcript.add_exon(to_merge.exons[0])
 
-        # Remove i+1 from list
-        transcripts.pop(i+1)
-    else: 
-        i=i+1
+        return transcripts
 
-    return merge_monoexonic(transcripts, i)    
+    return [transcripts, to_merge]
