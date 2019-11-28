c778acfe1ce84c8e6dc4e76175b27c44558b8679
Jacob Windsor <me@jcbwndsr.com>
2019-11-28 17:51:35 +0100

    Ensure faked transcript exons are sorted

--- 2.0/utils/fakers.py
+++ 2.0/utils/fakers.py
@@ -10,9 +10,9 @@ class Faker():
         self.chromosome_length = chromosome_length
 
     def exon(self, strand="+", start=None, length=50, transcript_suffix="", gene_suffix=""):
-        if not start:
+        if start is None:
             start = random.randrange(0, self.chromosome_length)
-
+            
         transcript_id = f"transcript_start{start}_length{length}_{transcript_suffix}"
         gene_id= f"gene_start{start}_length{length}_{gene_suffix}"
         return Exon(self.source_type, self.chromosome, start, start+length, strand, transcript_id, gene_id)
@@ -24,6 +24,7 @@ class Faker():
         for i in range(0, num_exons - len(exons_seed)):
             transcript.add_exon(self.exon(**kwargs))
 
+        transcript.exons.sort(key=lambda exon: exon.start)
         return transcript
 
     def contig(self, num_transcripts=10, transcripts_seed=None, **kwargs):
@@ -41,6 +42,6 @@ class Faker():
         length = kwargs.get("length") or 50
         for i in range(0, num_transcripts):
             transcripts_seed.append(self.transcript(1, None, start=start, **kwargs))
-            start = start + length - 2
+            start = start + length - 1
         
         return self.contig(num_transcripts, transcripts_seed)
