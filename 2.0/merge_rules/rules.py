f8341c8df9a1394d9e030e5448fe205ff5f33c33
Jacob Windsor <me@jcbwndsr.com>
2019-11-29 12:06:12 +0100

    Use iterators insteads of loops for get intro set

--- 2.0/merge_rules/rules.py
+++ 2.0/merge_rules/rules.py
@@ -2,12 +2,7 @@ def overlap(t1, t2):
     return t1.end > t2.start
 
 def _get_intron_set(t):
-    introns = []
-    for idx, item in enumerate(t.exons):
-        if idx >= len(t.exons) - 1:
-            break
-        introns.append((item.end, t.exons[idx+1].start))
-    return introns
+    return list(map(lambda exons: (exons[0].end, exons[1].start), zip(t.exons, t.exons[1:])))
 
 def same_introns(t1, t2):
     return _get_intron_set(t1) == _get_intron_set(t2)
