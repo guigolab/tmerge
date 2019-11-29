4a0c927cf5000e17b2efe38e9d0c280ec929ca15
Jacob Windsor <me@jcbwndsr.com>
2019-11-29 15:24:26 +0100

    Add rule for no extron intron overlap

--- 2.0/merge_rules/rules.py
+++ 2.0/merge_rules/rules.py
@@ -1,8 +1,33 @@
-def overlap(t1, t2):
-    return t1.end > t2.start
+from itertools import product
+from functools import reduce, partial
+
+def _coordinates_to_tuple(t):
+    return (t.start, t.end)
+
+_objs_to_coordinate_tuples = partial(map, _coordinates_to_tuple)
+
+def _overlap(t1, t2):
+    return t1[1] > t2[0] and t2[1] > t1[0]
 
 def _get_intron_set(t):
-    return list(map(lambda exons: (exons[0].end, exons[1].start), zip(t.exons, t.exons[1:])))
+    return map(lambda exons: (exons[0].end, exons[1].start), zip(t.exons, t.exons[1:]))
+
+def transcript_overlap(t1, t2):
+    return _overlap(_coordinates_to_tuple(t1), _coordinates_to_tuple(t2))
 
 def same_introns(t1, t2):
-    return _get_intron_set(t1) == _get_intron_set(t2)
+    return list(_get_intron_set(t1)) == list(_get_intron_set(t2))
+
+def no_exon_intron_overlap(t1, t2):
+    longer = t1 if len(t1.exons) > len(t2.exons) else t2
+    shorter = t1 if len(t1.exons) <= len(t2.exons) else t2
+
+    return all(
+        map(
+            lambda x: not _overlap(*x),
+            product(
+                _get_intron_set(shorter),
+                _objs_to_coordinate_tuples(longer.exons)
+            )
+        )
+    )
--- 2.0/merge_rules/rules_test.py
+++ 2.0/merge_rules/rules_test.py
@@ -1,5 +1,5 @@
 import unittest
-from merge_rules.rules import overlap, same_introns
+from merge_rules.rules import transcript_overlap, same_introns, no_exon_intron_overlap
 from utils.fakers import Faker
 import copy
 
@@ -7,14 +7,14 @@ class TestRules(unittest.TestCase):
     def setUp(self):
         self.faker = Faker("gtf", "chr1")
 
-    def test_overlap(self):
+    def test_transcript_overlap(self):
         t1 = self.faker.transcript(1, None, start=0, length=50)
         t2 = self.faker.transcript(1, None, start=49, length=50)
 
-        self.assertTrue(overlap(t1, t1))
+        self.assertTrue(transcript_overlap(t1, t1))
 
         t3 = self.faker.transcript(1, None, start=200)
-        self.assertFalse(overlap(t1, t3))
+        self.assertFalse(transcript_overlap(t1, t3))
 
     def test_same_introns(self):
         t1 = self.faker.transcript(10)
@@ -24,3 +24,18 @@ class TestRules(unittest.TestCase):
 
         t2.add_exon(self.faker.exon())
         self.assertFalse(same_introns(t1, t2))
+
+    def test_no_exon_intron_overlap(self):
+        t1 = self.faker.transcript(2)
+        t2 = copy.deepcopy(t1)
+
+        self.assertTrue(no_exon_intron_overlap(t1, t2))
+
+        t3 = copy.deepcopy(t1)
+        t3.exons[0].end += 50
+        t3.exons[1].start += 50
+        t3.exons[1].end += 50
+
+        self.assertFalse(no_exon_intron_overlap(t1, t3))
+
+        self.assertFalse(no_exon_intron_overlap(t3, t1))
