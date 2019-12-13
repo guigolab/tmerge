456d2a2d4ad39059de63b3d75bf30b9737835ebb
Jacob Windsor <me@jcbwndsr.com>
2019-12-13 16:15:28 +0100

    Build transcripts when parsing to increase speed

--- 2.0/importers/gtf.py
+++ 2.0/importers/gtf.py
@@ -1,17 +1,24 @@
 from models.exon import Exon
+from models.transcript import Transcript
 
 def parse(path):
-    sequences = []
+    transcripts = {}
     with open(path, "r") as f:
         for line in f:            
             try:
                 data = line.split("\t")
                 transcript_id = data[8].split(";")[1].replace("transcript_id", "").replace("\"", "").replace(" ", "")
                 gene_id = data[8].split(";")[0].replace("gene_id", "").replace("\"", "").replace(" ", "")
-                sequence = Exon("gtf", data[0], int(data[3]), int(data[4]), data[6], transcript_id, gene_id)
-                sequences.append(sequence)
+                exon = Exon("gtf", data[0], int(data[3]), int(data[4]), data[6], transcript_id, gene_id)
+                prev_transcript = transcripts.get(exon.transcript_id, None)
+                
+                if prev_transcript:
+                    prev_transcript.add_exon(exon)
+                else:
+                    transcripts[exon.transcript_id] = Transcript([exon])
+
             except Exception as e:
                 # TODO: Handle this exception
                 print(e)
                 pass
-    return sequences
+    return list(transcripts.values())
