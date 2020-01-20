66a7f9e310b7bab1f5357fd8d73ecc5acba9b06c
Jacob Windsor <me@jcbwndsr.com>
2020-01-20 17:47:37 +0100

    Fix test

--- 2.0/builders/merged_test.py
+++ 2.0/builders/merged_test.py
@@ -8,9 +8,8 @@ class MergedTest(unittest.TestCase):
 
     def test_overlapping_monoexonic(self):
         contig = self.faker.contig_overlapping_monoexonic()
-        merged = build([contig])
-        self.assertEqual(len(merged), 1)
-        self.assertEqual(len(merged[0].transcripts), 1)
-        self.assertEqual(contig.start, merged[0].start)
-        self.assertEqual(contig.end, merged[0].end)
-        self.assertEqual(len(contig.transcripts[0].exons), len(merged[0].transcripts[0].exons))
+        merged = build(contig)
+        self.assertEqual(len(merged.transcripts), 1)
+        self.assertEqual(contig.start, merged.start)
+        self.assertEqual(contig.end, merged.end)
+        self.assertEqual(len(contig.transcripts[0].exons), len(merged.transcripts[0].exons))
