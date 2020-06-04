9817ff738bf81e6cfd23adb2b6fef89123d0ab28
Jacob Windsor <me@jcbwndsr.com>
2020-06-04 13:13:03 +0200

    Fix read support plugin

--- 2.0/plugins/read_support.py
+++ 2.0/plugins/read_support.py
@@ -1,49 +1,79 @@
 from utils import ranges
 from functools import reduce
+from itertools import permutations, islice
 
 class ReadSupport():
-    def __init__(self, merger, end_fuzz = 50, min_read_support = 1):
+    def __init__(self, merger, end_fuzz = 0, min_read_support = 1, speed = False):
         self.end_fuzz = end_fuzz
         self.min_read_support = min_read_support
-        merger.hooks["contig_built"].tap(self.add_full_length_count)
-        merger.hooks["tm_merged"].tap(self.calc_full_length_count)
-        merger.hooks["contig_merged"].tap(self.remove_unsupported)
+        self.speed = speed
 
-    def add_full_length_count(self, transcripts):
-        for t in transcripts:
-            if "full_length_count" not in t.meta:
-                t.meta["full_length_count"] = 1
+        merger.hooks["transcript_added"].tap(self.transcript_added)
+        merger.hooks["contig_built"].tap(self.calc_support)
+
+    def add_full_length_count(self, transcript):
+        if "full_length_count" not in transcript.meta:
+            transcript.meta["full_length_count"] = 1
 
     def get_cum_intron_length(self, tm):
         return reduce(lambda x,y: x+y, map(lambda x: x[1] - x[0], tm), 0)
 
-    def calc_full_length_count(self, merge_result, merged):
-        # Note: merge_result is always the result of the merge so the TM with lower TSS and higher TES
-
-        # Get the junctions from merge_result that are before the TSS of merged
-        TSS_junctions = [j for j in merge_result.junctions if j[1] <= merged.TSS]
-        TSS_intron_length = self.get_cum_intron_length(TSS_junctions)
-        # Calculate the spliced distance between merged_result TSS and merged TSS
-        dist_TSS = merged.TSS - merge_result.TSS - TSS_intron_length
+    def supports_target(self, target, other):
+        if (
+            other.junctions == target.junctions and 
+            other.TSS >= target.TSS - self.end_fuzz and
+            other.TSS <= target.TSS + self.end_fuzz and
+            other.TES >= target.TES - self.end_fuzz and
+            other.TES <= target.TES + self.end_fuzz
+        ):
+            return True
         
-        # Get the junctions from merge_result that are after the TES of merged
-        TES_junctions = [j for j in merge_result.junctions if j[0] >= merged.TES]
-        TES_intron_length = self.get_cum_intron_length(TES_junctions)
-        # Calculate the spliced distance between merged TES and merged_result TES
-        dist_TES = merge_result.TES - merged.TES - TES_intron_length
+        return False
         
-        # merge_result.meta["full_length_count"] += merged.meta.get("full_length_count", 0)
-        if dist_TSS <= self.end_fuzz and dist_TES <= self.end_fuzz:
-            merge_result.meta["full_length_count"] += 1
+        # return
+        # # Get the junctions from target that are before the TSS of other
+        # TSS_junctions = [j for j in target.junctions if j[1] <= other.TSS]
+        # TSS_intron_length = self.get_cum_intron_length(TSS_junctions)
+        # # Calculate the spliced distance between target TSS and other TSS
+        # dist_TSS = other.TSS - target.TSS - TSS_intron_length
+        
+        # # Get the junctions from target that are after the TES of other
+        # TES_junctions = [j for j in target.junctions if j[0] >= other.TES]
+        # TES_intron_length = self.get_cum_intron_length(TES_junctions)
+        # # Calculate the spliced distance between other TES and target TES
+        # dist_TES = target.TES - other.TES - TES_intron_length
+        
+        # if dist_TSS < self.end_fuzz and dist_TES < self.end_fuzz:
+        #      target.meta["full_length_count"] += 1
 
-    def remove_unsupported(self, transcripts):
-        if self.min_read_support == 1:
+    def transcript_added(self, transcript):
+        self.add_full_length_count(transcript)          
+
+    def calc_support(self, transcripts):
+        if self.speed and self.min_read_support == 1:
+            # Don't bother computing if min read support is 1
             return
+        supports = set()
+        
+        for i, target in enumerate(transcripts):
+            if self.speed and target.id in supports:
+                # Ignore transcripts that support another transcript. This forgoes precision slightly but with a great speed increase
+                if target.meta["full_length_count"] < self.min_read_support:
+                    target.remove()
+                continue
+            
+            for other in transcripts[i+1:]:
+                if other.TSS - self.end_fuzz > target.TSS:
+                    # Since input is ordered, can break early if other comes after target
+                    break
+                
+                if self.supports_target(target, other):
+                    target.meta["full_length_count"] += 1
+                    supports.add(other.id)
 
-        to_remove = []
-        for t in transcripts:
-            if t.meta["full_length_count"] < self.min_read_support:
-                to_remove.append(t)
-
-        for t in to_remove:
-            transcripts.remove(t)
+                if self.speed and target.meta["full_length_count"] >= self.min_read_support:
+                    # If full length count already exceeds min read support, can break early
+                    break
+            
+            if target.meta["full_length_count"] < self.min_read_support:
+                target.remove()
