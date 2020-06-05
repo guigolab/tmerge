9e8b483a3ba9c48570b77f2c0382b163f50b69b9
Jacob Windsor <me@jcbwndsr.com>
2020-06-05 21:16:14 +0200

    Update CLI help

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,6 +1,6 @@
 import getopt, argparse
 from plugins import Stats, ReadSupport, SplicedLengths, MergedInfo, SpliceSiteScoring
-from src import Merge
+from src import _Merge
 
 """
 tmerge CLI
@@ -8,28 +8,28 @@ tmerge CLI
 Runs tmerge as a CLI with stats, read support, spliced lengths, merged info and splice site scoring plugins enabled.
 """
 def main():
-    description = "tmerge 2.0 Beta"
+    description = "tmerge. Merge transcriptome read-to-genome alignments into non-redundant transcript models."
     parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument("-i", "--input", help="Input GTF file")
     parser.add_argument("-o", "--output", help="Output GTF file")
     parser.add_argument("-s", "--stats", action="store_true", help="Provide statistics for merged transcripts.")
-    parser.add_argument("--tolerance", help="Exon overhang tolerance", type=int, default=0)
-    parser.add_argument("--min_read_support", help="Minimum read support", type=int, default=0)
-    parser.add_argument("--end_fuzz", help="end fuzz", type=int, default=0)
+    parser.add_argument("--tolerance", help="Maximum number of nucleotides of terminal exon overhang allowed within an intron of another transcript during the merging of input reads", type=int, default=0)
+    parser.add_argument("--min_read_support", help="Minimum number of times a read alignment (by exon/intron structure) needs to be present in the input. Any read alignments below this threshold will be removed", type=int, default=0)
+    parser.add_argument("--end_fuzz", help="Tolerated fuzziness of 5' and 3' ends for two reads to be considered equivalent when calculating read support", type=int, default=0)
     parser.add_argument("--speed", help="Speed mode. Enables options that forgoe sensitivity and precision for faster merge time.", action="store_true")
     parser.add_argument("--processes", help="The number of processes (threads) allowed to run. Must be greater than 2. If left unspecified, then will use the maximum number available.", type=int, default=None)
-    parser.add_argument("--splice_scoring", action="store_true")
-    parser.add_argument("--acceptor_path")
-    parser.add_argument("--donor_path")
-    parser.add_argument("--fasta_path")
-    parser.add_argument("--valid_acceptor", type=int, default=4)
-    parser.add_argument("--valid_donor", type=int, default=4)
+    parser.add_argument("--splice_scoring", action="store_true", help="Enable splice site scoring.", default=False)
+    parser.add_argument("--acceptor_path", help="Only if splice_scoring enabled. Path to the acceptor file.")
+    parser.add_argument("--donor_path", help="Only if splice_scoring enabled. Path to the donor file.")
+    parser.add_argument("--fasta_path", help="Only if splice_scoring enabled. Path to the FASTA genome file.")
+    parser.add_argument("--valid_acceptor", type=int, default=4, help="Only if splice_scoring enabled. Threshold at which transcripts are removed.")
+    parser.add_argument("--valid_donor", type=int, default=4, help="Only if splice_scoring enabled. Threshold at which transcripts are removed.")
 
 
     args = parser.parse_args()
 
-    merger = Merge(args.input, args.output, args.tolerance, args.processes)
+    merger = _Merge(args.input, args.output, args.tolerance, args.processes)
 
     """
     Load plugins
