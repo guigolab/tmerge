78ab6f53273e31ab214df5f60dd22d21fe00db59
Jacob Windsor <me@jcbwndsr.com>
2020-06-09 22:18:03 +0200

    Use a mix of min_abundance and min_read_support

--- plugins/read_support.py
+++ plugins/read_support.py
@@ -2,31 +2,35 @@ from functools import reduce
 
 class ReadSupport():
     """
-    Compute read support for each transcript and filter by min_read_support.
+    Computes read support and abundance for each transcript and filters transcripts that do not meet min_abundance or min_read_support.
 
-    Read support is the number of reads in the input (as defined by the exon/intron structure) that match a gien transcript.
+    Read support: number of reads in the input (as defined by the exon/intron structure) that match a gien transcript.
+    abundance: read support as a fraction of the maximum read support in a contig.
 
     Parameters
     ----------
     hooks: dict
     end_fuzz: int
         Tolerated fuzziness of 5' and 3' ends for two reads to be considered equivalent when computing read support
+    min_abundance: float
+        Minimum abundance for a transcript. Any transcripts with abundance below this threshold will be removed.
     min_read_support: int
-        Minimum number of reads that must support a given transcript. Any transcripts with read support below this threshold will be removed.
-    speed: bool
-        When True, favour speed over sensitivity & precision
+        Minimum read support for a transcript
     """
-    def __init__(self, hooks, end_fuzz = 0, min_read_support = 1, speed = False, **kwargs):
-        self.end_fuzz = end_fuzz
-        self.min_read_support = min_read_support
-        self.speed = speed
+    def __init__(self, hooks, end_fuzz = 0, min_abundance = 0.15, min_read_support = 1, **kwargs):
+        if min_abundance > 1:
+            raise TypeError("min_abundance must be < 1.")
 
-        hooks["transcript_added"].tap(self.transcript_added)
+        self.end_fuzz = end_fuzz
+        self.min_abundance = min_abundance
+        self.min_read_support = min_read_support
+
+        hooks["transcript_added"].tap(self.add_meta)
         hooks["contig_built"].tap(self.calc_support)
 
-    def add_full_length_count(self, transcript):
-        if "full_length_count" not in transcript.meta:
-            transcript.meta["full_length_count"] = 1
+    def add_meta(self, transcript):
+        transcript.meta["read_support"] = 1
+        transcript.meta["abundance"] = 0
 
     def supports_target(self, target, other):
         if (
@@ -38,22 +42,12 @@ class ReadSupport():
         ):
             return True
         
-        return False
-
-    def transcript_added(self, transcript):
-        self.add_full_length_count(transcript)          
+        return False 
 
     def calc_support(self, transcripts):
-        if self.speed and self.min_read_support == 1:
-            # Don't bother computing if min read support is 1
-            return
-        supports = set()
-        
+        max_read_support = 1
         for i, target in enumerate(transcripts):
-            if self.speed and target.id in supports:
-                # Ignore transcripts that support another transcript. This forgoes precision slightly but with a great speed increase
-                if target.meta["full_length_count"] < self.min_read_support:
-                    target.remove()
+            if  target.removed:
                 continue
             
             for other in transcripts[i+1:]:
@@ -61,13 +55,20 @@ class ReadSupport():
                     # Since input is ordered, can break early if other comes after target
                     break
                 
+                if other.removed:
+                    continue
+                
                 if self.supports_target(target, other):
-                    target.meta["full_length_count"] += 1
-                    supports.add(other.id)
+                    target.meta["read_support"] += 1
+                    other.remove() # Remove if supports target as is the same TM
 
-                if self.speed and target.meta["full_length_count"] >= self.min_read_support:
-                    # If full length count already exceeds min read support, can break early
-                    break
-            
-            if target.meta["full_length_count"] < self.min_read_support:
+            if target.meta["read_support"] > max_read_support:
+                max_read_support = target.meta["read_support"]
+
+            if target.meta["read_support"] < self.min_read_support:
                 target.remove()
+
+        for t in transcripts:
+            t.meta["abundance"] = t.meta["read_support"] / max_read_support
+            if t.meta["abundance"] < self.min_abundance:
+                t.remove()
