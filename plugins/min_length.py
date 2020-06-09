5a6f844a82d15d02b3b30c7beb0cfb856b4afbaa
Jacob Windsor <me@jcbwndsr.com>
2020-06-09 22:17:35 +0200

    Fix typos

--- plugins/min_length.py
+++ plugins/min_length.py
@@ -1,12 +1,12 @@
 class MinLength:
-    def __init__(self, hooks, min_length=200):
+    def __init__(self, hooks, min_length=200, **kwargs):
         """
         Remove TMs that do not satisfy min_length.
         """
         self.min_length = min_length
         hooks["contig_merged"].tap(self.remove_short_transcripts)
 
-    def remove_short_transcript(self, transcripts):
+    def remove_short_transcripts(self, transcripts):
         for t in transcripts:
             if t.length < self.min_length:
                 t.remove()
