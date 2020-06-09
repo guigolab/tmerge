269e3fb8e9d741a2e44ea0a68a2c5b2d3c28e12b
Jacob Windsor <me@jcbwndsr.com>
2020-06-09 22:16:41 +0200

    Organize args

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,5 +1,5 @@
 import getopt, argparse
-from plugins import Stats, ReadSupport, SplicedLengths, MergedInfo, SpliceSiteScoring, MinLength
+from plugins import Stats, ReadSupport, SplicedLengths, MergedInfo, SpliceSiteScoring, MinLength, Depth
 from src import merge
 
 """
@@ -11,15 +11,27 @@ def main():
     description = "tmerge. Merge transcriptome read-to-genome alignments into non-redundant transcript models."
     parser = argparse.ArgumentParser(description=description)
 
+    # Basic options passed to merger and not for plugins
     parser.add_argument("-i", "--input", help="Input GTF file")
     parser.add_argument("-o", "--output", help="Output GTF file")
-    parser.add_argument("-s", "--stats", action="store_true", help="Provide statistics for merged transcripts.")
-    parser.add_argument("--tolerance", help="Maximum number of nucleotides of terminal exon overhang allowed within an intron of another transcript during the merging of input reads", type=int, default=0)
-    parser.add_argument("--min_read_support", help="Minimum number of times a read alignment (by exon/intron structure) needs to be present in the input. Any read alignments below this threshold will be removed", type=int, default=1)
-    parser.add_argument("--end_fuzz", help="Tolerated fuzziness of 5' and 3' ends for two reads to be considered equivalent when calculating read support", type=int, default=0)
-    parser.add_argument("--min_length", help="The minimum length required for output transcripts.", default=200)
-    parser.add_argument("--speed", help="Speed mode. Enables options that forgoe sensitivity and precision for faster merge time.", action="store_true")
     parser.add_argument("--processes", help="The number of processes (threads) allowed to run. Must be greater than 2. If left unspecified, then will use the maximum number available.", type=int, default=None)
+    parser.add_argument("--tolerance", help="Maximum number of nucleotides of terminal exon overhang allowed within an intron of another transcript during the merging of input reads", type=int, default=0)
+
+    # Stats
+    parser.add_argument("-s", "--stats", action="store_true", help="Provide statistics for merged transcripts.")
+
+    # Options for MinLength
+    parser.add_argument("--min_length", help="The minimum length required for output transcripts.", default=200, type=int)
+
+    # Options for Depth
+    parser.add_argument("--trim", help="Trim output transcript ends? Ends are trimmed when there is a sudden drop in depth.", default=True)
+
+    # Options for ReadSupport
+    parser.add_argument("--min_abundance", help="Minimum abundance for transcripts", type=float, default=0.15)
+    parser.add_argument("--min_read_support", help="Minimum number of times a read alignment (by exon/intron structure) needs to be present in the input.", type=int, default=1)
+    parser.add_argument("--end_fuzz", help="Tolerated fuzziness of 5' and 3' ends for two reads to be considered equivalent when calculating read support", type=int, default=0)
+    
+    # Options for SpliceSiteScoring
     parser.add_argument("--splice_scoring", action="store_true", help="Enable splice site scoring.", default=False)
     parser.add_argument("--acceptor_path", help="Only if splice_scoring enabled. Path to the acceptor file.")
     parser.add_argument("--donor_path", help="Only if splice_scoring enabled. Path to the donor file.")
